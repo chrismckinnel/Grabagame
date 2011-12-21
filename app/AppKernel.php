65babbd044930bc8014d044fda1c442149dbff3a
Chris McKinnel <chrismckinnel@gmail.com>
2011-12-21 21:45:22 +0000

    Generated Grabagame bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Grabagame\Bundle\GrabagameBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
