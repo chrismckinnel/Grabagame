b42f96b1ff941272717be88255cd0169f0badb32
Chris McKinnel <chrismckinnel@gmail.com>
2012-05-06 21:39:43 +0100

    Travis

--- deployment/project-setup.sh
+++ deployment/project-setup.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-mv src/app/config/parameters.ini.dist src/app/config/parameters.ini
-php src/bin/vendors install
-php src/app/console doctrine:schema:create
-sudo chmod -R 777 src/app/logs
-sudo chmod -R 777 src/app/cache
+sudo pear install phpunit/DbUnit
+cd src
+mv app/config/parameters.ini.dist src/app/config/parameters.ini
+php bin/vendors install
+php app/console doctrine:schema:create
+sudo chmod -R 777 app/logs
+sudo chmod -R 777 app/cache
