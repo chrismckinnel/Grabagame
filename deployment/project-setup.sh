e5a093284c6b313fc463142eed952e59a4621c96
Chris McKinnel <chrismckinnel@gmail.com>
2012-05-06 21:27:12 +0100

    More travis changes

--- deployment/project-setup.sh
+++ deployment/project-setup.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+php src/app/console doctrine:schema:create
+mv src/app/config/parameters.ini.dist src/app/config/parameters.ini
 php src/bin/vendors install
 sudo chmod -R 777 src/app/logs
 sudo chmod -R 777 src/app/cache
