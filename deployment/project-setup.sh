319e115112fd5452b6f20a295b331e744239db16
Chris McKinnel <chrismckinnel@gmail.com>
2012-05-06 21:28:47 +0100

    Travis

--- deployment/project-setup.sh
+++ deployment/project-setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-php src/app/console doctrine:schema:create
 mv src/app/config/parameters.ini.dist src/app/config/parameters.ini
 php src/bin/vendors install
+php src/app/console doctrine:schema:create
 sudo chmod -R 777 src/app/logs
 sudo chmod -R 777 src/app/cache
