c4c4b09ac052b20d17c215704b2b21603a7b8d01
Chris McKinnel <chrismckinnel@gmail.com>
2012-05-07 22:37:24 +0100

    Travis tweaks

--- deployment/project-setup.sh
+++ deployment/project-setup.sh
@@ -2,8 +2,12 @@
 pyrus install http://phptal.org/latest.tar.gz
 phpenv rehash
 pyrus install phpunit/DbUnit
+sudo mkdir /var/log/grabagame
+sudo mkdir /var/cache/grabagame
+sudo chmod -R 777 /var/log/grabagame
+sudo chmod -R 777 /var/cache/grabagame
 cd src
-mv app/config/parameters.ini.dist app/config/parameters.ini
+mv app/config/parameters.ini.travis app/config/parameters.ini
 php bin/vendors install
 php app/console doctrine:schema:create
 sudo chmod -R 777 app/logs
