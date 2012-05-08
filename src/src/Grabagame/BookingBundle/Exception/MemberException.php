a541a30c8c545c1228045a64d2811a55447e72af
Chris McKinnel <chris.mckinnel@tangentlabs.co.uk>
2012-05-08 19:41:59 +0100

    Testing member service!

---
+++ src/src/Grabagame/BookingBundle/Exception/MemberException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Grabagame\BookingBundle\Exception;
+
+/**
+ * Custom exception class for a member exception
+ *
+ * @package Grabagame\BookingBundle\Exception
+ * @author  Chris McKinnel <chris.mckinnel@tangentlabs.co.uk>
+ */
+class MemberException extends \Exception
+{
+}
