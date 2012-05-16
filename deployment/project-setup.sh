e5b5e1787452e6cf2578e220440d7d48f01bf3ba
Chris McKinnel <chrismckinnel@gmail.com>
2012-05-17 00:29:53 +0100

    Travis tests

--- deployment/project-setup.sh
+++ deployment/project-setup.sh
@@ -8,9 +8,8 @@ sudo chmod -R 777 /var/log/grabagame
 sudo chmod -R 777 /var/cache/grabagame
 cd src
 mv app/config/parameters.ini.travis app/config/parameters.ini
+cat app/config/parameters.ini
 php bin/vendors install
-cat app/config/parameteres.ini
-php app/console doctrine:schema:create --dump-sql
 php app/console doctrine:schema:create
 sudo chmod -R 777 app/logs
 sudo chmod -R 777 app/cache
