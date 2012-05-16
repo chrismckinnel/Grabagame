3eeb5bc850a3b14875e40ed5f7cb195e3334a21d
Chris McKinnel <chris.mckinnel@tangentlabs.co.uk>
2012-05-16 20:44:26 +0100

    Added trace to exception log, make menu sprite transparent

--- src/src/Grabagame/BookingBundle/Controller/RegistrationController.php
+++ src/src/Grabagame/BookingBundle/Controller/RegistrationController.php
@@ -7,50 +7,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse,
 
 class RegistrationController extends BaseController
 {
-    public function registerAction()
-    {
-        $form = $this->container->get('fos_user.registration.form');
-        $formHandler = $this->container->get('fos_user.registration.form.handler');
-        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
 
-        $process = $formHandler->process($confirmationEnabled);
-        if ($process) {
-            $user = $form->getData();
-
-            if ($confirmationEnabled) {
-                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
-                $route = 'fos_user_registration_check_email';
-            } else {
-                $this->authenticateUser($user);
-                $route = 'fos_user_registration_confirmed';
-                $view = 'userConfirmed';
-                $bindings = array('User' => $user);
-            }
-
-            $registrationFlashMessage = $this->renderView('FOSUserBundle:Registration:'.$view.'.html.twig', $bindings);
-            $this->setFlash('alert-success', $registrationFlashMessage);
-            $url = $this->container->get('router')->generate($route);
-
-            return new RedirectResponse($url);
-        }
-
-        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
-            'form' => $form->createView(),
-            'theme' => $this->container->getParameter('fos_user.template.theme'),
-        ));
-    }
-
-    /**
-     * Returns a rendered view.
-     *
-     * @param string $view       The view name
-     * @param array  $parameters An array of parameters to pass to the view
-     *
-     * @return string The renderer view
-     */
-    public function renderView($view, array $parameters = array())
-    {
-        return $this->container->get('templating')->render($view, $parameters);
-    }
 
 }
